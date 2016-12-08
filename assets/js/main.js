1893f8318fb7b621f5cc50531878e6778d1c0292
Vitaliy <raxmind@gmail.com>
2016-12-09 01:55:31 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -292,7 +292,7 @@
 					var $this = $(this);
 
 					// Close.
-						$('<div class="close">Close</div>')
+						$('<div class="close">Закрыть</div>')
 							.appendTo($this)
 							.on('click', function() {
 								location.hash = '';
