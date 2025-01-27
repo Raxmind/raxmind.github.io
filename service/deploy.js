ad7f258a8bd1c0256b1a06cdfdcf4f2062dd12af
Vitaliy <raxmind@gmail.com>
2025-01-27 15:52:37 +0200

    Update and rename deploy.js to northflank.js

--- service/deploy.js
+++
@@ -1,6 +0,0 @@
-(function() {
-	'use strict';
-
-	Lampa.Utils.putScriptAsync(['https://zenrlsvg.deploy.cx/online.js'], function() {});
-
-})();
---
+++ service/northflank.js
@@ -0,0 +1,6 @@
+(function() {
+	'use strict';
+
+	Lampa.Utils.putScriptAsync(['https://p01--lampac--6792cxpk8bx8.code.run/online.js'], function() {});
+
+})();
