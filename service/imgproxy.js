10dace07b1980aac3c1ec5c4944be16e57813d34
Vitaliy <raxmind@gmail.com>
2025-01-30 06:17:05 +0200

    Update imgproxy.js

--- service/imgproxy.js
+++ service/imgproxy.js
@@ -1,29 +1,28 @@
-(function() {
+(function () {
     'use strict';
 
-        // Прокси-домен
-        const proxyUrl = "https://cors.fenixx04412.workers.dev/";
+    // Прокси-домен
+    var proxyUrl = "https://cors.fenixx04412.workers.dev/";
 
-        // Домены, которые нужно проксировать
-	const targetDomains = ["kinopoiskapiunofficial.tech", "st.kp.yandex.net", "avatars.mds.yandex.net"];
+    // Домены, которые нужно проксировать
+    var targetDomains = ["kinopoiskapiunofficial.tech", "st.kp.yandex.net", "avatars.mds.yandex.net"];
 
-        // Функция для добавления прокси только к картинкам с конкретных адресов
-        function addProxy(url) {
-            if (url.includes(targetDomains)) {
-		if (url.startsWith(targetDomains) && (url.endsWith('.jpg') || url.endsWith('.png'))) {
-    		return proxyUrl + url;
-	}
-		}
-            return url; // Оставляем остальные URL без изменений
-        }
-
-        // Перехват запросов к изображениям
-        Lampa.Listener.follow('image', function(event) {
-            if (event.url) {
-                const originalUrl = event.url;
-                event.url = addProxy(event.url); // Проксируем, если нужно
-                console.log(`Обработано изображение: ${originalUrl} -> ${event.url}`);
+    // Функция для добавления прокси только к картинкам с конкретных адресов
+    function addProxy(url) {
+        for (var i = 0; i < targetDomains.length; i++) {
+            if (url.indexOf(targetDomains[i]) !== -1 && (url.indexOf('.jpg') !== -1 || url.indexOf('.png') !== -1)) {
+                return proxyUrl + url;
             }
-        });
+        }
+        return url; // Оставляем остальные URL без изменений
+    }
 
+    // Перехват запросов к изображениям
+    Lampa.Listener.follow('image', function (event) {
+        if (event.url) {
+            var originalUrl = event.url;
+            event.url = addProxy(event.url); // Проксируем, если нужно
+            console.log("Обработано изображение: " + originalUrl + " -> " + event.url);
+        }
+    });
 })();
