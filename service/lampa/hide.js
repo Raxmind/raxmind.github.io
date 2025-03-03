5abe06a0387b026eef0b72838ddb03d62df50cf4
Vitaliy <raxmind@gmail.com>
2025-03-03 12:42:31 +0200

    Update hide.js

--- service/lampa/hide.js
+++ service/lampa/hide.js
@@ -1,17 +1,16 @@
 (function () {
-    'use strict';	
-   var garbage_collector = {
-      name: 'Сборщик хлама',
-      version: '1.0',
-      description: 'Убирает не нужные разделы как торренты и лента'
-    };
-		Lampa.Listener.follow('app',(e)=>{
+    'use strict';
+Lampa.Listener.follow('app',(e)=>{
         if(e.type == 'ready'){
 			setTimeout(function(){
 				$("[data-action=mytorrents]").eq(0).remove();
 				$("[data-action=feed]").eq(0).remove();
-			},10); 
+				$("[data-action=persons]").eq(0).remove();
+				$("[data-action=anime]").eq(0).remove();
+				$("[data-action=subscribes]").eq(0).remove();
+				$("[data-action=timetable]").eq(0).remove();
+				$("[data-action=console]").eq(0).remove();
+			},10);
         }
     });
-
 })();
