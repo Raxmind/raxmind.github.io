42dd13717ebf3bf8a2268463a231e6dec28e3ba4
Vitaliy <raxmind@gmail.com>
2025-03-08 06:26:06 +0200

    Update ui.js

--- service/lampa/ui.js
+++ service/lampa/ui.js
@@ -7,10 +7,10 @@
 				$(".head__logo-cap").remove();
 				$(".head__logo-halloween").remove();
 				$(".open--broadcast").remove();
+				$(".full-start__status").remove();
 				$("[data-action=anime]").eq(0).remove();
 				$("[data-action=timetable]").eq(0).remove();
 				$("[data-action=console]").eq(0).remove();
-				$(".full-start__status").eq(0).remove();
 			},10);
         }
     });
